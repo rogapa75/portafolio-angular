045cbc24a20b5d2c071f4e74973e6c8421714274
Roger Gala <rogapa@hotmail.com>
2020-03-25 00:12:44 -0600

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -11,18 +11,32 @@ export class InfoPaginaService {
   info:infoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) {
 
-   // console.log('Servicio de infoPagina Listo');
-    
+   this.cargarInfo();
+   this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: infoPagina) => {
-          // 
           this.cargada = true;
           this.info = resp;
           console.log(resp);
           // console.log( resp['twitter'] );
 
-      });
+      }); 
   }
+
+  private cargarEquipo(){
+    this.http.get('https://curso-angula-html.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
+        this.equipo = resp;
+        console.log(resp);
+    })
+
+  }
+  
 }
