4a9f5238601c2d686e9b8e0e36d1a5b98006d33d
Roger Gala <rogapa@hotmail.com>
2020-03-26 23:28:29 -0600

    Fin de la seecion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -4,6 +4,7 @@ import { Producto } from '../interfaces/producto.interface';
 
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,6 +12,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) { 
 
@@ -19,19 +21,67 @@ export class ProductosService {
 
   private cargarProductos(){
 
-    this.http.get('https://curso-angula-html.firebaseio.com/productos_idx.json')
+    return new Promise ((resolve, reject) => {
+ this.http.get('https://curso-angula-html.firebaseio.com/productos_idx.json')
     .subscribe((resp: Producto[]) => {
         console.log(resp);
         this.productos = resp;
-        
-
-        setTimeout(() => {
-          this.cargando = false;  
-        }, 2000);
-
+        this.cargando = false;
+        resolve();
     });
-      
+
+   
+
+        // para verificar que funcione el Load
+        // setTimeout(() => {
+        //   this.cargando = false;  
+        // }, 2000);
+
+    });     
   }
 
+  getProducto(id: string){
+
+    // ``````
+    return this.http.get(`https://curso-angula-html.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto(termino: string){
+
+    if (this.productos.length == 0){
+      // cargar productos
+      this.cargarProductos().then( () => {
+        // Ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos(termino);
+      })
+    }else {
+      // aplicar filtro
+      this.filtrarProductos(termino);
+    }
+
+    // this.productosFiltrado = this.productos.filter(Producto => {
+    //   return true;
+    // } );
+
+    // console.log(this.productosFiltrado);
+
+
+  }
+
+  private filtrarProductos(termino : string){
+    this.productosFiltrado= [];
+
+    termino = termino.toLocaleLowerCase();
+      // console.log(this.productos);
+      this.productos.forEach( prod => {
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+          if(prod.categoria.indexOf( termino) >= 0 || tituloLower.indexOf( termino) >= 0
+          ){
+            this.productosFiltrado.push(prod);
+          }
+      });
+
+  }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InfoPaginaService } from '../../services/info-pagina.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,18 @@ export class HeaderComponent implements OnInit {
 
   
   // constructor(public infoPaginaService: InfoPaginaService) { }
-  constructor(public _servicio: InfoPaginaService) { }
+  constructor(public _servicio: InfoPaginaService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  buscarProducto(termino: string) {
+      // console.log(termino);
+
+if(termino.length < 1){
+  return;
+}
+this.router.navigate(['/search', termino]);
   }
 
 }
